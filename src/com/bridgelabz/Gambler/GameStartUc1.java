f71a91069e2558f6566cca21478f424cc57135a9
softramesh29 <softramesh29@gmail.com>
2022-04-20 08:31:16 +0530

    Added : Gambler project added with UC1 file

---
+++ src/com/bridgelabz/Gambler/GameStartUc1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Gambler;
+
+public class GameStartUc1 {
+	public static void main(String[] args) {
+		int everyDayStake = 100;
+		int everyGameBet = 1;
+		System.out.println("Player having Stake of every is "+ everyDayStake+ " Dollar ");
+		System.out.println("Bet for Each game is "+ everyGameBet+ " Dollar ");
+		
+	}
+}
